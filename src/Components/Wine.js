a04b80d8ccfd0cd884fc3e0664287ed9d101369c
juliamachin <juliaamachin@gmail.com>
2022-06-07 15:43:36 -0700

    created a useState for the type

--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 
 export default function Wine(props) {
   const [wineData, setWineData] = useState([]);
+  const [initialType, setInitialType] = useState("");
+
+  
 
   useEffect(() => {
+    setInitialType(props.type)
     fetch(`http://localhost:8080/wines`)
       .then((response) => response.json())
       .then((data) => setWineData(data.wines));
@@ -12,10 +16,14 @@ export default function Wine(props) {
   console.log(wineData);
 
   const wines = wineData.map((item) => {
-    console.log(item.Producer)
-    return <li>{item.Producer}</li>;
+    //console.log(initialType)
+    if(item.Type === initialType) {
+      return(
+        <li>{item.Producer}</li>
+      )
+    }
   });
-
+console.log(wines)
   return (
     <div>
       <h1>{props.type}</h1>
