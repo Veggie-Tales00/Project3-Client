bf444cccd24996e714822618b4ea455e2df44100
Anas Ryadi <anasryadi@Anass-MBP.socal.rr.com>
2022-06-07 16:34:13 -0700

    created fetch and map for the dishes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,47 +3,55 @@ import { Link } from "react-router-dom";
 import { Button, Card, CardImg, CardTitle, Input } from "reactstrap";
 
 const Home = (props) => {
-
-
   return (
     <div>
       <h1>Som-base</h1>
       <Card>
         <CardTitle tag="h3">Red Wine</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Link to='/wine'>
-          <Button id="Red" onClick={props.setType}>Go to</Button>
+        <Link to="/wine">
+          <Button id="Red" onClick={props.setType}>
+            Go to
+          </Button>
         </Link>
       </Card>
 
       <Card>
         <CardTitle tag="h3">White</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Link to='/wine'>
-          <Button id="White" onClick={props.setType}>Go to</Button>
+        <Link to="/wine">
+          <Button id="White" onClick={props.setType}>
+            Go to
+          </Button>
         </Link>
       </Card>
 
       <Card>
         <CardTitle tag="h3">Sparkling</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Link to='/wine'>
-          <Button id="Sparkling" onClick={props.setType}>Go to</Button>
+        <Link to="/wine">
+          <Button id="Sparkling" onClick={props.setType}>
+            Go to
+          </Button>
         </Link>
       </Card>
 
       <Card>
         <CardTitle tag="h3">Desert / Other</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Link to='/wine'>
-          <Button id="Desert" onClick={props.setType}>Go to</Button>
+        <Link to="/wine">
+          <Button id="Desert" onClick={props.setType}>
+            Go to
+          </Button>
         </Link>
       </Card>
 
       <Card>
         <CardTitle tag="h3">Dishes</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Button >Go to</Button>
+        <Link to="/dish">
+          <Button>Go to</Button>
+        </Link>
       </Card>
     </div>
   );
