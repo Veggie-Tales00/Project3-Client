c31efc9dd6319e4687ba697167770696f31cfd1c
juliamachin <juliaamachin@gmail.com>
2022-06-07 15:32:48 -0700

    producers displayed

--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -8,23 +8,22 @@ export default function Wine(props) {
       .then((response) => response.json())
       .then((data) => setWineData(data.wines));
   }, []);
- 
- console.log(wineData)
- 
- const redWines = wineData.filter = () => {
-   return (
-     <li></li>
-   )
- }
- 
-    return (
-        <div>
-            <h1>{props.type}</h1>
-            <form>
-              <input type="text" placeholder="Search..." />
-              <input type="Submit" />
-            </form>
-        </div>
-    )
-}
 
+  console.log(wineData);
+
+  const wines = wineData.map((item) => {
+    console.log(item.Producer)
+    return <li>{item.Producer}</li>;
+  });
+
+  return (
+    <div>
+      <h1>{props.type}</h1>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <input type="Submit" />
+      </form>
+      <ul>{wines}</ul>
+    </div>
+  );
+}
