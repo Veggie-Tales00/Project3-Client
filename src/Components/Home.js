1648f245e59935dce073b48b6394c45418a07132
Jacob <jacob.d.wencloff@gmail.com>
2022-06-07 14:48:17 -0700

    Data from home component is passed to the Wine component through App.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, CardImg, CardTitle, Input } from "reactstrap";
 
-const Home = () => {
+const Home = (props) => {
+
+
   return (
     <div>
       <h1>Som-base</h1>
@@ -10,7 +12,7 @@ const Home = () => {
         <CardTitle tag="h3">Red Wine</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
         <Link to='/wine'>
-          <Button>Go to</Button>
+          <Button id="Red" onClick={props.setType}>Go to</Button>
         </Link>
       </Card>
 
@@ -18,7 +20,7 @@ const Home = () => {
         <CardTitle tag="h3">White</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
         <Link to='/wine'>
-          <Button>Go to</Button>
+          <Button id="White" onClick={props.setType}>Go to</Button>
         </Link>
       </Card>
 
@@ -26,7 +28,7 @@ const Home = () => {
         <CardTitle tag="h3">Sparkling</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
         <Link to='/wine'>
-          <Button>Go to</Button>
+          <Button id="Sparkling" onClick={props.setType}>Go to</Button>
         </Link>
       </Card>
 
@@ -34,14 +36,14 @@ const Home = () => {
         <CardTitle tag="h3">Desert / Other</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
         <Link to='/wine'>
-          <Button>Go to</Button>
+          <Button id="Desert" onClick={props.setType}>Go to</Button>
         </Link>
       </Card>
 
       <Card>
         <CardTitle tag="h3">Dishes</CardTitle>
         <CardImg alt="" src="" width=""></CardImg>
-        <Button>Go to</Button>
+        <Button >Go to</Button>
       </Card>
     </div>
   );
--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Wine() {
-  return (
-    <div>Wine</div>
-  )
+export default function Wine(props) {
+
+    
+    return (
+        <div>
+            <h1>{props.type}</h1>
+        </div>
+    )
 }
