fdafe127f509063c1df2d2723748efff3966c3ef
Anas Ryadi <anasryadi@Anass-MBP.socal.rr.com>
2022-06-06 18:40:14 -0700

    added links to employee and manager

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -10,16 +10,6 @@ const Links = () => {
           <h2>Home</h2>
         </Button>
       </Link>
-      <Link to="/manager">
-        <Button color="primary">
-          <h2>Manager</h2>
-        </Button>
-      </Link>
-      <Link to="/employee">
-        <Button color="primary">
-          <h2>Employee</h2>
-        </Button>
-      </Link>
     </nav>
   );
 };
--- src/Components/Manager.js
+++ src/Components/Manager.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'reactstrap'
 
 const Manager = () => {
   return (
-    <div>Manager</div>
+    <div>
+      <h1>Manager</h1>
+      <Link to="/manager/edit">
+        <Button color="primary">
+          <h2>Add Wine</h2>
+        </Button>
+      </Link>
+      <Link to="/manager/edit">
+        <Button color="primary">
+          <h2>Delete Wine</h2>
+        </Button>
+      </Link>
+      <Link to="/manager/edit">
+        <Button color="primary">
+          <h2>Add Dish</h2>
+        </Button>
+      </Link>
+      <Link to="/manager/edit">
+        <Button color="primary">
+          <h2>Update Wine</h2>
+        </Button>
+      </Link>
+    </div>
   )
 }
 
