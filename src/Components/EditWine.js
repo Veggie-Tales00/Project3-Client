5c649b2ec87e0e5df92776236f255a41d950fbe6
Jacob <jacob.d.wencloff@gmail.com>
2022-06-08 15:02:39 -0700

    Displaying wine data into a basic list

--- src/Components/EditWine.js
+++ src/Components/EditWine.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import apiURL from '../apiURL'
+import { useEffect } from 'react'
+import { List } from 'reactstrap'
 
 export default function EditWine() {
+const [wineList, setWineList] = useState([])
+
+    useEffect(()=>{
+        fetch(`${apiURL}/wines`)
+        .then(response => response.json())
+        .then(data => setWineList(data.wines))
+        .catch(()=> console.log('Error'))
+
+    }, [])
+
+    const displayList = wineList.map((item)=>{
+        return(
+            <li key={item._id}>
+                <div>
+                    Type: {item.Type}
+                </div>
+                <h3>{item.Producer}</h3>
+                <h4>{item.Variety}</h4>
+            </li>
+        )
+    })
+    console.log(wineList)
   return (
-    <div>EditWine</div>
+    <div>
+        EditWine
+        <List type="unstyled">
+            {displayList}
+        </List>
+    </div>
   )
 }
