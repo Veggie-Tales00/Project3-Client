c2bef4dfa3815aeec2ec7ba0ce752d45b4daf57f
Jacob <jacob.d.wencloff@gmail.com>
2022-06-09 16:32:35 -0700

    Implimented CRUD to edit and Delete any given data entry in the Wine DB

--- src/Components/EditWine.js
+++ src/Components/EditWine.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-import { Accordion, AccordionBody, AccordionItem, List, Button } from 'reactstrap'
+import { Accordion, AccordionBody, AccordionItem, List, Button, Form, Input } from 'reactstrap'
 import apiUrl from '../apiURL'
 
 export default function EditWine() {
     const [wineList, setWineList] = useState([])
     const [isThisOpen, setIsThisOpen] = useState('')
+    const [openEdit, setOpenEdit] = useState('')
+    const [type, setType] = useState('')
+    const [producer, setProducer] = useState('')
+    const [variety, setVariety] = useState('')
+    const [bottlePrice, setBottlePrice] = useState('')
+    const [glassPrice, setGlassPrice] = useState('')
+    const [uploadData, setUploadData] = useState({})
+    const [PutId, setPutId] = useState({})
 
     useEffect(() => {
         fetch(`${apiUrl}/wines`)
@@ -15,38 +23,114 @@ export default function EditWine() {
 
     }, [])
 
+    //setting request options for the potential delete request
     const delRequestOp = {
         method: 'DELETE',
-        headers: {'Content-Type': 'application/json'}
-      }
+        headers: { 'Content-Type': 'application/json' }
+    }
+
 
     const handleExpansion = (e) => {
-        // console.log(e)
-        if(e.target.innerText === "Edit" || e.target.innerText === "Delete"){
+        // console.log(e.target.innerText)
 
-          let targetObjectID =  e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id
+        //this if statement determins what is being clicked on inside the accordian
+        //if it is either the delete or edit button, the accordian will stay open, otherwise it will close
+        if (e.target.innerText === "Edit" || e.target.innerText === "Delete") {
 
-            if(e.target.innerText === "Delete"){
-                let tempCopy = [...wineList]
+            //This is a very ... very long abstraction used to determin the DB object id associated with
+            //the curent datapoint you are attempting to preform CRUD on
+            let targetObjectID = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id
+
+            if (e.target.innerText === "Delete") {
+                let tempCopy = []
                 fetch(`${apiUrl}/wines/${targetObjectID}`, delRequestOp)
-                .then(response => response.json())
-                .then(data => tempCopy.push(data))
-                .then(()=> setWineList(tempCopy))
-                console.log(tempCopy)
+                    .then(response => response.json())
+                    .then(data => tempCopy = data.items)
+                    .then(() => setWineList(tempCopy))
+
+            } else if (e.target.innerText === "Edit") {
+                setOpenEdit(e.target.id)
+                console.log('open')
+                setPutId(targetObjectID)
             }
-        }else{
+
+        } else {
             if (isThisOpen === '') {
                 setIsThisOpen(parseInt(e.target.id))
+                console.log(e.target)
             } else {
                 setIsThisOpen('')
             }
         }
+    }
 
+
+    let obj = {}
+    const handleInput = (e) => {
+        e.preventDefault()
+
+        let text = e.target.value
+        let target = e.target.id
+
+
+        if (target === 'type') {
+            setType(text)
+        } else if (target === 'producer') {
+            setProducer(text)
+        } else if (target === 'variety') {
+            setVariety(text)
+        } else if (target === 'bottle') {
+            setBottlePrice(parseInt(text))
+        } else if (target === 'glass') {
+            setGlassPrice(parseInt(text))
+        }
+    }
+    console.log(type)
+    console.log(producer)
+
+
+    const handleSub = () => {
+        if (type !== '') {
+            obj.Type = type
+        }
+        if (producer !== '') {
+            obj.Producer = producer
+        }
+        if (variety !== '') {
+            obj.Variety = variety
+        }
+        if (bottlePrice !== '') {
+            obj.Price.Bottle = bottlePrice
+        }
+        if (glassPrice !== '') {
+            obj.Price.Glass = glassPrice
+        }
+
+        console.log(obj)
+
+
+
+
+        const postRequestOp = {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
+        }
+
+        fetch(`${apiUrl}/wines/${PutId}`, postRequestOp)
+            .then(response => response.json())
+            .then(data => {
+                setWineList(data.wines)
+
+            })
+            .catch(() => console.log('error'))
+
+
+        setOpenEdit('')
 
     }
-  };
 
-    const displayList = wineList.map((item, i) => {
+    const displayList1 = wineList.map((item, i) => {
         return (
             <li key={item._id} id={item._id} onClick={handleExpansion} >
                 <Accordion open={isThisOpen}>
@@ -61,22 +145,40 @@ export default function EditWine() {
                                     <List >
                                         <li>Bottle:{item.Price.Bottle}</li>
                                         <li>Glass:{item.Price.Glass}</li>
-                                        <Button >Edit</Button>
+                                        <Button id={item._id} >Edit</Button>
                                         <Button >Delete</Button>
                                     </List>
                                 </List>
                             </AccordionBody>
                         </AccordionItem>
                     </div>
+                    <Accordion open={openEdit}>
+                        <AccordionItem>
+                            <AccordionBody accordionId={item._id}>
+                                <Form>
+                                    <Input id="type" type="text" placeholder={`Type: ${item.Type}`} onChange={handleInput}></Input>
+                                    <Input id="producer" type="text" placeholder={`Producer: ${item.Producer}`} onChange={handleInput}></Input>
+                                    <Input id="variety" type="text" placeholder={`Variety: ${item.Variety}`} onChange={handleInput}></Input>
+                                    <Input id="bottle" type="text" placeholder={`Bottle Price: ${item.Price.Bottle}`} onChange={handleInput}></Input>
+                                    <Input id="glass" type="text" placeholder={`Glass Price: ${item.Price.Glass}`} onChange={handleInput}></Input>
+                                    <Button id={item._id} onClick={handleSub}>Submit</Button>
+                                </Form>
+                            </AccordionBody>
+                        </AccordionItem>
+                    </Accordion>
                 </Accordion>
             </li>
         )
     })
-    // console.log(wineList)
+
+
+
+
+
     return (
         <div>
             <List type="unstyled">
-                {displayList}
+                {displayList1}
             </List>
         </div>
     )
