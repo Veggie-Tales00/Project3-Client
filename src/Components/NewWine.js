9cdecee34919fd69bfac5942bf57f20499c995f6
Jacob <jacob.d.wencloff@gmail.com>
2022-06-08 18:40:54 -0700

    Added an alert to verify entered data before submitting to the DB

--- src/Components/NewWine.js
+++ src/Components/NewWine.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import apiUrl from "../apiURL";
 import { useState } from 'react'
-import {Form, Input, Button} from 'reactstrap'
+import {Form, Input, Button, Alert} from 'reactstrap'
 
 const NewWine = () => {
 const [type, setType] = useState('')
@@ -11,23 +11,30 @@ const [producer, setProducer] = useState('')
 const [vintage, setVintage] = useState('')
 const [price, setPrice] = useState({Bottle: null, Glass: null})
 const [note, setNotes] = useState([])  
+const [uploadData, setUploadData] = useState({})
+const [toggleAlertContent, setAlertContent] = useState(false)
 
 //function to handle the Submit button event
-  const handleSub = (e) =>{
+  const handleSubForm = (e) =>{
 
     console.log(e.target.form.id)
 
     //building the JSON object that will be used in the postRequestOp object
-    const uploadData = {
+    setUploadData ({
       Price: price,
+      Location: location,
       Type: type,
       Variety: variety,
       Producer: producer,
       Vintage: vintage,
       Notes: note
-    }
+    })
     console.log(uploadData)
 
+    toggleConfirmContent()
+  }
+
+  const sendEntry = () =>{
     //Setting the POST request options to be used in the POST request
     const postRequestOp = {
       method: 'POST',
@@ -35,11 +42,15 @@ const [note, setNotes] = useState([])
       body: JSON.stringify(uploadData)
     }
 
+
     //Calling the POST request
     fetch(`${apiUrl}/wines`, postRequestOp)
       .then(response => response.json())
       .then(data => console.log(data))
+      .then(()=>{setAlertContent(false)})
       .catch(()=> console.log('error'))
+
+
   }
 
   //Handling the individual input feilds
@@ -80,6 +91,9 @@ const [note, setNotes] = useState([])
     // console.log(price)
   }
 
+  const toggleConfirmContent = () =>{
+      setAlertContent(true)
+  }
   return (
     <div>
     <Form>
@@ -90,8 +104,22 @@ const [note, setNotes] = useState([])
         <Input id="Vintage" type='text' placeholder='Wine Vintage' onChange={handleInput}></Input>
         <Input id="Price" type='text' placeholder='Wine Price (Bottle, Glass)' onChange={handleInput}></Input>
         <Input id="Notes" type='text' placeholder='Wine Notes(1, 2, 3, ...)' onChange={handleInput}></Input>
-        <Button onClick={handleSub}> Submit </Button>
+        <Button onClick={handleSubForm}> Submit </Button>
     </Form>
+    <div id="Verify">
+      <Alert dismissible color="warning" isOpen={toggleAlertContent}>
+        Is This Data Correct? <br /><br />
+        Wine Type: {type}<br />
+        Wine Variety: {variety}<br />
+        Wine Vintage: {vintage}<br />
+        Wine Location: {location}<br />
+        Wine Producer: {producer}<br />
+        Bottle Price: {price.Bottle}<br />
+        Glass Price: {price.Glass}<br />
+        Notes: {note}<br />
+        <Button onClick={sendEntry}>Send Data</Button>
+      </Alert>
+    </div>
 </div>
   )
 }
