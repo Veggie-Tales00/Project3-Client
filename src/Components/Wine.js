333d585cf9c8355419f8f46703069238587dc991
juliamachin <juliaamachin@gmail.com>
2022-06-11 18:04:41 -0700

    .

--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
+<<<<<<< HEAD
 export default function Wine(props) {
 
     
@@ -8,4 +9,19 @@ export default function Wine(props) {
             <h1>{props.type}</h1>
         </div>
     )
+=======
+function Wine() {
+  //const [wineData, setWineData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/wines`)
+      .then((response) => response.json())
+      .then((data) => console.log(data.wines));
+  }, []);
+ 
+
+  return <div>Wine</div>;
+>>>>>>> b4e849c (fetched api from localhost)
 }
+
+export default Wine;
